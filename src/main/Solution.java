5ec7fa8fb90ce20248ea3b60359da408bec080f5
Aline Korshunova <aline.korshunova00@gmail.com>
2021-07-24 20:57:28 +0300

    Реализован фабричный метод getPerson

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,9 +1,14 @@
 package main;
 
+import male.MaleFactory;
+
 /** Точка входа в приложение **/
 
 public class Solution {
     public static void main(String[] args) {
-
+        MaleFactory maleFactory = new MaleFactory();
+        System.out.println(maleFactory.getPerson(99));
+        System.out.println(maleFactory.getPerson(4));
+        System.out.println(maleFactory.getPerson(15));
     }
 }
---
+++ src/male/MaleFactory.java
@@ -0,0 +1,17 @@
+package male;
+
+import main.Human;
+
+/** Фабрика мужчин XD **/
+
+public class MaleFactory {
+    public Human getPerson(int age){ //фабричный метод
+        if (age <= KidBoy.MAX_AGE)
+            return new KidBoy();
+
+        else if (age <= TeenBoy.MAX_AGE)
+            return new TeenBoy();
+
+        else return new Man();
+    }
+}
